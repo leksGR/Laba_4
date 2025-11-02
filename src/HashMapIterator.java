d36f22aea12457902137372b6493d3728bac52cc
MiesLies17-23 <vadimtolstouhov@gmail.com>
2025-11-02 11:28:27 +0300

    Добавил комментарии

--- src/HashMapIterator.java
+++ src/HashMapIterator.java
@@ -2,22 +2,41 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * Класс итератора для HashMap
+ */
 public class HashMapIterator extends HashMap {
 
+    /** Статический объект интерфейса итератор */
     private static Iterator<Map.Entry<String, Key>> _iterator;
 
+    /**
+     * Конструктор с параметрами
+     */
     HashMapIterator(HashMap<String, Key> hm) {
         _iterator  = hm.entrySet().iterator();
     }
 
+    /**
+     * Метод, возвращающий следующий элемент структуры
+     * @return элемент структуры
+     */
     public Map.Entry<String, Key> nextKey() {
         return _iterator.next();
     }
 
+    /**
+     * Метод проверяет существование следующего элемента
+     * @return true если существует, false - в противном случае
+     */
     public boolean hasNextKey() {
         return _iterator.hasNext();
     }
 
+    /**
+     * Метод возвращает длину структуры
+     * @return длина HashMap
+     */
     public int length() {
         int length = 0;
         while (hasNextKey()) {
