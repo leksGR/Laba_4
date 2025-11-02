805eb7678646294f7c51df7349ca132619e86829
MiesLies17-23 <vadimtolstouhov@gmail.com>
2025-11-02 11:27:25 +0300

    Добавил меню и выборку действий, но не зациклил

--- src/UI.java
+++ src/UI.java
@@ -20,7 +20,7 @@ public class UI {
      */
     private void getInformationMessage() {
         System.out.println("Лабораторная работа №4");
-        System.out.println("Бригада 13: Медведев А., Толстоухов В.");
+        System.out.println("Бригада 13: Медведев А., Толстоухов В.\n");
     }
 
     /**
@@ -28,26 +28,94 @@ public class UI {
      */
     public void run() {
         getInformationMessage();
-
+        menu();
         try {
-            boolean success = OutputWriter.saveToFile("keys.txt");
-
-            if (success) {
-                System.out.println("Сохранено в файл keys_report.txt");
-            } else {
-                System.out.println("Ошибка при сохранении в файл");
+            int choose = Integer.parseInt(_sc.nextLine());
+            switch (choose) {
+                case 1:
+                    addNewKey();
+                    break;
+                case 2:
+                    showAllKeys();
+                    break;
+                case 3:
+                    uploadToFile();
+                    break;
+                case 0:
+                    System.out.println("Вы вышли из программы!");
+                    return;
+                default:
+                    System.out.println("Выбор не распознан!");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * Меню приложения
+     */
     private void menu() {
         System.out.println("Выберите действие: ");
-        System.out.println("1. Добавить пару ключ-значение");
+        System.out.println("1. Добавить новый ключ");
         System.out.println("2. Вывести содержимое репозитория");
         System.out.println("3. Сохранить содержимое репозитория в файл");
-        System.out.println("Ваш выбор: ");
-        int choose = Integer.parseInt(_sc.nextLine());
+        System.out.println("0. Выход");
+        System.out.print("Ваш выбор: ");
+    }
+
+    /**
+     * Метод, инициализирующий добавление нового ключа в HashMap
+     */
+    private void addNewKey() {
+        System.out.print("Введите значение ключа: ");
+        String value = _sc.nextLine();
+        Key.putNewByValue(value);
+        System.out.println("Ключ успешно добавлен!");
+        showAllKeys();
+    }
+
+    /**
+     * Метод выводит все ключи HashMap
+     */
+    private void showAllKeys() {
+        String message = Key.showAll();
+        System.out.println(message);
+    }
+
+    /**
+     * Метод, инициализирующие сохранение данных в файл
+     */
+    private void uploadToFile() {
+        System.out.print("Введите название файла: ");
+        String filename = _sc.nextLine();
+        boolean success = OutputWriter.isFileExist(filename);
+
+        if (success) {
+            System.out.println("Сохранено в файл " + filename);
+        } else {
+            System.out.println("Ошибка! Данный файл не существует.\n" +
+                    "Хотите создать файл и сохранить туда информация о ключах?");
+            System.out.println("1. Да");
+            System.out.println("2. Нет");
+            System.out.print("Ваш выбор: ");
+            int choose = Integer.parseInt(_sc.nextLine());
+
+            switch(choose) {
+                case 1:
+                    success = OutputWriter.saveToFile(filename);
+                    if (success) {
+                        System.out.println("Сохранено в файл " + filename);
+                    } else {
+                        System.out.println("Ошибка сохранения в файл " + filename);
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Выбор не распознан!");
+                    break;
+            }
+        }
     }
 }
